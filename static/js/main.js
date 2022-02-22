8a9ab64fd939eb2a4c3566a3fa75a235a9c02872
A-Bolf <hosc19@gmail.com>
2022-02-22 11:29:36 +0100

    add new board creation feature

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,36 @@
 import {boardsManager} from "./controller/boardsManager.js";
+import {domManager} from "./view/domManager.js";
 
-function init() {
-    boardsManager.loadBoards();
+function close() {
+    this.parentElement.parentElement.parentElement.classList.remove("is-visible")
+}
+
+const initModalButtons = () => {
+    const openEls = document.querySelectorAll("[data-open]");
+    for (const el of openEls) {
+        el.addEventListener("click", function () {
+            const modalId = this.dataset.open;
+            document.getElementById(modalId).classList.add("is-visible");
+        });
+    }
+
+const isVisible = "is-visible";
+
+document.addEventListener("click", e => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+    domManager.addEventListener(
+        `[data-close]`,
+        "click",
+        close)
+}
+
+
+async function init() {
+    await boardsManager.loadBoards();
+    initModalButtons()
 }
 
 init();
