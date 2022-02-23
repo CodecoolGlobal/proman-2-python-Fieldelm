eb10f35a7b23f9952b8a56ad9bd49748a9f031be
fieldelm <debreka@gmail.com>
2022-02-23 16:24:31 +0100

    make an input field and rename card only in javaScript

--- static/js/controller/cardsManager.js
+++ static/js/controller/cardsManager.js
@@ -5,24 +5,25 @@ import {domManager} from "../view/domManager.js";
 export let cardsManager = {
     loadCards: async function (boardId) {
         const cards = await dataHandler.getCardsByBoardId(boardId);
-        console.log(cards)
+
         for (let card of cards) {
             const cardBuilder = htmlFactory(htmlTemplates.card);
             const content = cardBuilder(card);
-
-
-            domManager.addEventListener(
-                `.card[data-card-id="${card.id}"]`,
-                "click",
-                deleteButtonHandler
-            );
-            domManager.addEventListener(
-                `.card[data-card-id="${card.id}"]`,
-                "click", (e)=> {
-                    updateTitle(e.currentTarget)
-                }
-            );
             domManager.addChild(`#board${boardId}column${card['status_id']}`, content);
+
+            domManager.addEventListener(
+                `#card${card.id}`,
+                "click", (e)=>{
+                    console.log(e.currentTarget)
+                    if(e.currentTarget.childNodes.length===5) {
+                        updateTitle(e.currentTarget)
+                        console.log(e.currentTarget.childNodes.length)
+                    }
+                }
+
+
+            );
+
             // domManager.addEventListener(
             //     `.card[data-card-id="${card.id}"]`,
             //     "click",
@@ -35,9 +36,24 @@ export let cardsManager = {
 function deleteButtonHandler(clickEvent) {
 
 }
-function  updateTitle(card){
-    const inputField = document.createElement("input");
-    inputField.type = "text";
-    card.appendChild(inputField);
-    const newTitle = inputField.value
-    console.log(newTitle)
+function  updateTitle(card) {
+
+        const oldTitleDiv = card.querySelector(".card-title")
+        const oldTitle = oldTitleDiv.innerText
+        console.log(oldTitleDiv)
+        oldTitleDiv.innerText = ""
+        let newTitleInput = document.createElement("input");
+        newTitleInput.value = oldTitle;
+        card.type = "submit"
+        card.appendChild(newTitleInput);
+
+        newTitleInput.addEventListener("keydown", ev => {
+            if (ev.code === 'Enter'){
+
+                oldTitleDiv.innerText = newTitleInput.value
+                newTitleInput.remove()
+
+            }
+        })
+
+}
